08aa2e051ef35dc9e21cf311e847e21f2c8840e9
ViktorHotsiy <viktorhotsiy@gmail.com>
2023-05-19 19:03:47 +0300

    Blank Project Push

---
+++ Source/FPControlla/FPControllaGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FPControllaGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FPCONTROLLA_API AFPControllaGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
